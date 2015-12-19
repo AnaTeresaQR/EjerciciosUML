7b18fb3475c8d9a41adef14ecd0bacb64378ba70
AnaTeresaQR <ana.quesada@ucrso.info>
2015-12-18 22:08:08 -0600

    Se crea la clase cliente con su funcionalidad

---
+++ Sistema1/ejerciciosUMLSistema1/src/sistema1/Cliente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema1;
+
+/**
+ *
+ * @author Ana Teresa
+ */
+public class Cliente extends Persona {
+
+    private String telefono;
+
+    public Cliente(String nombre, int edad, String telefono) {
+        super(nombre, edad);
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", telefono: " + telefono;
+    }
+
+}
