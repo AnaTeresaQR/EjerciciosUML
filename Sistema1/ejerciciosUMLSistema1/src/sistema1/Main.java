7e75c911bec581b0b697df6ca1381fcdf0127f43
AnaTeresaQR <ana.quesada@ucrso.info>
2015-12-29 16:46:14 -0600

    edición de el Main
    Se agregan otros datos de prueba

--- Sistema1/ejerciciosUMLSistema1/src/sistema1/Main.java
+++ Sistema1/ejerciciosUMLSistema1/src/sistema1/Main.java
@@ -16,11 +16,16 @@ public class Main {
         Empleado emp1 = new Empleado("pepito", 30, 120000, "Directivo");
 
         Empleado empSub1 = new Empleado("juan", 45, 110000, "Subordinado");
-        
+
         Empleado empSub2 = new Empleado("carlos", 50, 90000, "Subordinado");
+
+        Empleado empSub3 = new Empleado("santiago", 56, 16000, "Subordinado");
 
         emp1.agregarSub(empSub1);
         emp1.agregarSub(empSub2);
+
+        empSub2.agregarSub(empSub3);
+
         System.out.println("" + emp1.toString());
 
     }
